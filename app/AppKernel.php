c25211a899357c149e53817da14d1c0e93659da7
Hetphil <p_bordmann@orange.fr>
2016-08-04 09:32:29 +0200

    css corps onglets page achat

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,7 +18,6 @@ class AppKernel extends Kernel
             new Louvre\BilletterieBundle\LouvreBilletterieBundle(),
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new FOS\JsRoutingBundle\FOSJsRoutingBundle(),
-            new ADesigns\CalendarBundle\ADesignsCalendarBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
